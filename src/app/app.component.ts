3c2e5cf38d68207c6c3af217fcd49d587af91141
Oscar Alonso Rueda Ruiz <138699802+oruedarr@users.noreply.github.com>
2024-06-26 15:00:59 -0500

    added toastr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ssr';
+
+  constructor(private toastr: ToastrService)
+  {
+
+  }
+
+  showSuccess() {
+    this.toastr.success('Hello world!', 'Toastr fun!');
+  }
 }
