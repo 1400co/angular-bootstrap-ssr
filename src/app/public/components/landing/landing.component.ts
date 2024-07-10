0df07615298042e243c86c0a40f1031cc67a9654
Oscar Alonso Rueda Ruiz <138699802+oruedarr@users.noreply.github.com>
2024-07-09 21:46:25 -0500

    styling and landing with hero

--- src/app/public/components/landing/landing.component.ts
+++ src/app/public/components/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-landing',
@@ -9,12 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export default class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private toastr: ToastrService) { }
 
   ngOnInit(): void {
-  
+
   }
 
-
+  showToastr() {
+    this.toastr.success('Hello world!', 'Toastr fun!');
+  }
 
 }
