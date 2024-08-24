2f45296220c0dcead79f3e375f75d7125561cef9
Oscar Alonso Rueda Ruiz <138699802+oruedarr@users.noreply.github.com>
2024-08-23 19:58:40 -0500

    menu de ejemplo

--- src/app/private/layout/private-header/private-header.component.ts
+++ src/app/private/layout/private-header/private-header.component.ts
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './private-header.component.scss'
 })
 export class PrivateHeaderComponent {
+  isToggled = false;
 
+  toggleSidebar() {
+    this.isToggled = !this.isToggled;
+    const wrapper = document.getElementById('wrapper');
+    if (wrapper) {
+      if (this.isToggled) {
+        wrapper.classList.add('toggled');
+      } else {
+        wrapper.classList.remove('toggled');
+      }
+    }
+  }
 }
